4388d200218ebf088367670a5c103f15b2c84116
Lincoln Pires <me@lincolnpires.com>
2022-09-11 19:48:45 +0200

    Make split functions work

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,64 @@
 import './App.css';
-
+import { useState } from 'react';
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { Footer } from './Footer.jsx';
 import { Header } from './Header.jsx';
 
-const firebaseConfig = {
+// const firebaseConfig = {
+//
+// }
+//
+// const app = initializeApp(firebaseConfig)
+// const fireStore = getFirestore(app)
 
-}
-
-const app = initializeApp(firebaseConfig)
-const fireStore = getFirestore(app)
-
-function ChatRoom() {
+function ChatRoom(props) {
   return (
       <>
-        Hello
+        Hello, {props.user.name}
       </>
   );
 }
 
-function SignIn() {
+function SignIn(props) {
+  const [name, setName] = useState('');
+
+  async function createUser(e) {
+    e.preventDefault();
+    const user = {name};
+    props.setUser(user);
+  }
+
   return (
       <>
         <h2>Sign In</h2>
-        <form>
+        <form onSubmit={createUser}>
           <label htmlFor="Name">Name</label>
-          <input type="text" placeholder="Name" required />
+          <input type="text" placeholder="Name" required value={name}
+                 onChange={async (e) => setName(e.target.value)} />
           <label htmlFor="email">Email</label>
           <input type="email" name="email" id="email" />
           <label htmlFor="password">Password</label>
           <input type="password" name="password" id="password" />
-          <button onClick={() => {}}>Sign In</button>
+          <button type="submit" disabled={!name}>Sign In</button>
         </form>
       </>
-  )
+  );
 }
 
 function App() {
-    let user = null;
+  const [user, setUser] = useState(null);
+  console.log(user);
 
-    return (
-    <div className="App">
-      <Header />
-      <h1>Vite + React</h1>
-      <div className="card">
-        {user ? <ChatRoom /> : <SignIn />}
+  return (
+      <div className="App">
+        <Header />
+        <div className="card">
+          {user ? <ChatRoom user={user} /> : <SignIn setUser={setUser} />}
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,15 @@
 import reactLogo from './assets/react.svg';
 
 export function Header() {
-  return <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" className="logo" alt="Vite logo" />
-    </a>
-    <a href="https://reactjs.org" target="_blank">
-      <img src={reactLogo} className="logo react" alt="React logo" />
-    </a>
-  </div>;
+  return <>
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://reactjs.org" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+    </div>
+    <h1>Vite + React</h1>
+  </>;
 }
